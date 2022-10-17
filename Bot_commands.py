9aa358a4b1fcf49a8e4d755bd81ddca3730415e2
Jmomanyi <jjjmomanyi@gmail.com>
2022-10-17 13:59:51 +0100

    knkdk

--- Bot_commands.py
+++ Bot_commands.py
@@ -1,25 +1,20 @@
 #!/bin/python3
 import bot as Bot
 import socket as s
+import bot1 as Bot1
 
 
 #class to handle bot commands
 class commands():
    
-    def command_library(command):
-       
-        if command=="!Help":
-            Bot.send_message(f"hello, I am {Bot.nickname} !"
-                             +"i can take the following commands:"
-                             +"!Help"
-                             +"!Hello"
-                             +"slap")
-        if command=="!Hello":
-            Bot.send_message("hello")    
-        if command=="!Slap":
-            Bot.send_message("slap")
-        else:
-            Bot.send_message("I don't know this command")  
+    def help(usr):
+        Bot1.bot_replies.send_message("Hello I am a bot"
+              +"i can take the following commands "+
+              "___________________________________"+
+              "1. !help - displays this message"+
+              "2. !roll - rolls a dice"+
+              "3. !flip - flips a coin"+
+              "4.!slap - slaps a user")
            
     
       
