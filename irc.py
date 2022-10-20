75d04c72ef59b799684c3035267db54f27ea428a
Jmomanyi <jjjmomanyi@gmail.com>
2022-10-20 16:54:51 +0100

    khfknr

--- irc.py
+++ irc.py
@@ -36,7 +36,7 @@ class client(Thread):
         self.sock = socket
         self.addr = address
         self.nickname = ""
-        self.user = ""
+        self.username = ""
         self.channel = []
         self.start()
 
@@ -44,8 +44,8 @@ class client(Thread):
         #ensure that all login information is received before continuing
         try:
             #While username and nickname not set
-            while self.nickname == "" and self.user == "":              
-                while self.user == "":
+            while self.nickname == "" and self.username == "":              
+                while self.username == "":
                     # Read received data from the client
                     message = self.sock.recv(1024).decode()
                     for line in message.splitlines():
@@ -59,7 +59,7 @@ class client(Thread):
                                     self.sock.send(b'Password: ')
                                     #nb: 2**10 is the max size of a message
                                     password = self.sock.recv(1024).decode()
-                                    if password == "admin":                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+                                    if password == "admin":                                                           
                                         self.nickname = messageParsed[1]
                                     elif password == "admin" and messageParsed[1] in client_list:
                                         self.sock.send(b'Nickname is already in use')
@@ -82,13 +82,17 @@ class client(Thread):
                         # Check if the client is sending a username parameter
                         if(messageParsed[0] == "USER"):
                             if(messageParsed[1] != ""):
-                                self.user = messageParsed[1]
+                                self.username = messageParsed[1]
+                            elif(messageParsed[1] == client.username):
+                                self.sock.send(b'Username is already in use')
+                                self.sock.close()
+                                return
                             else:
                                 self.sock.send(b'Error: Please enter a username')
              
-            print(self.user + " has joined the server" + " with the nickname " + self.nickname)
-            if(self.nickname != "" and self.user != ""):
-                print("Adding " + self.user + " to client list...")
+            print(self.username + " has joined the server" + " with the nickname " + self.nickname)
+            if(self.nickname != "" and self.username != ""):
+                print("Adding " + self.username + " to client list...")
                 try:
                     client_list.append(self)
                     print("Client list updated")
@@ -97,7 +101,7 @@ class client(Thread):
 
                 # Constructing and sending the initial welcome messages
                 msg1 = ':' + host + ' 001 ' + self.nickname + ' :Welcome to Group_10 server!\n'
-                message = msg1 + "Join general by typing /join #test\n"
+                message = msg1 + "Join test by typing /join #test\n"
                                 
                 self.sock.send(message.encode())
   
@@ -120,7 +124,6 @@ class client(Thread):
                             if(messageParsed[1] == channel):
                                 found = True                 
                         channel = messageParsed[1]
-
                         #If channel not found, create channel with name
                         if(not found):
                             channel_list.append(channel)
@@ -128,24 +131,24 @@ class client(Thread):
 
                         #If channel found, send reply codes and broadcast to everyone in channel
                         if (found):
-                            #200 and 300 are reply codes,that the server uses to inform the client that the command was successful
+                            #200 and 300 are reply codes,to inform the user which commands were executed
                             self.channel.append(channel)
                             chmsg1 = ':' + host + ' 200 ' + self.nickname + ' ' + channel + ' :No topic is set\n'
-                            chmsg2 = ':' + host + ' 300 ' + self.nickname + ' = ' + channel + ' :'
-                            inforserver = "NICK: " + self.nickname + " USERNAME: " + self.user + ' @ ' + self.addr[0] + '\n' + platform.node() + ' JOINED ' + channel + '\n'
+                            chmsg2 = ':' + host + ' 300 ' + self.nickname + 'Users in ' + channel + ' :'
+                            inforserver = "NICK: " + self.nickname + " USERNAME: " + self.username + ' @ ' + self.addr[0] + '\n' + platform.node() + ' JOINED ' + channel + '\n'
 
                             #Add every client in channel to list of users in channel 
                             for client in client_list:
                                 for clientChannel in client.channel:
                                     if(clientChannel == channel):
-                                        chmsg2 = chmsg2 + ' ' + client.nickname
+                                        chmsg2 = chmsg2 + ' ' + client.nickname          
                             chmsg2 = chmsg2 + '\n'
 
                             #Send channel reply codes 
                             chan_list = ':' + host + ' 366 ' + self.nickname + ' ' + channel + ' :End of NAMES list\n'
-                            REPLY = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                            REPLY = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                             message = REPLY + chmsg1 + chmsg2 + chan_list
-                            servermsg = inforserver + chmsg1
+                            servermsg = inforserver + chmsg1 + chmsg2
                             print(servermsg)
 
                             #send a message to clients in the channel
@@ -158,7 +161,7 @@ class client(Thread):
                     if(messageParsed[0] == "PART"):
                         if(self.channel):
                             channel = messageParsed[1]
-                            message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                            message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                             #For each client in channel, send message that user left
                             for client in client_list:
                                 for clientChannel in client.channel:
@@ -172,12 +175,13 @@ class client(Thread):
                     if(messageParsed[0] == "QUIT"):
                         #For each client, send message that user has quit
                         for client in client_list:
-                            message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                            message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                             client.sock.send(message.encode())
                         #Remove user from list of clients and then close the socket
                         client_list.remove(self)
                         print(self.nickname + " has quit")
                         self.sock.close()
+                        continue
 
                     #Message protocol
                     if(messageParsed[0] == "PRIVMSG"):
@@ -188,7 +192,7 @@ class client(Thread):
                             for clientChannel in client.channel:
                                 if(clientChannel == channel):
                                     if (client != self):
-                                        message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                                        message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                                         client.sock.send(message.encode())
 
 
@@ -197,7 +201,8 @@ class client(Thread):
                             if(messageParsed[1] == client.nickname):
                                 if(messageParsed[2] != ":"):
                                     if(messageParsed[1] != self.nickname):
-                                        message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                                        #Send message to user
+                                        message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                                         client.sock.send(message.encode())
                                         print(self.nickname + " sent a private message to " + client.nickname)
 
@@ -218,18 +223,19 @@ class client(Thread):
 
                     #user priviledges protocol
                     if(messageParsed[0] == "KICK"):
-                        if(self.user == "admin"):
+                        if(self.username == "admin"):
                             channel = messageParsed[1]
                             for client in client_list:   
                                if(messageParsed[2] == client.nickname):
-                                    message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                                    message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                                     client.sock.send(message.encode())
                                     print(self.nickname + " kicked " + client.nickname + " from " + channel)
                                     client.channel.remove(channel)
+                             #inform all users in channel that user was kicked       
                             for client in client_list:
                                 for clientChannel in client.channel:
                                     if(clientChannel == channel):
-                                        message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                                        message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                                         client.sock.send(message.encode())
                         else:
                             message = ':' + host + ' 481 ' + self.nickname + ' :Permission Denied- You do not have access to this command\n'
@@ -237,11 +243,11 @@ class client(Thread):
 
                     #admin  can be able to ban a user from a channel, names of users banned from a channels are added to a banned users and cannot rejoin
                     if(messageParsed[0] == "BAN"):
-                        if(self.user == "admin"):
+                        if(self.username == "admin"):
                             channel = messageParsed[1]
                             for client in client_list:
                                 if(messageParsed[2] == client.nickname):
-                                    message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                                    message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                                     client.sock.send(message.encode())
                                     print(self.nickname + " banned " + client.nickname + " from " + channel)
                                     banned_users.append(client.nickname)
@@ -249,15 +255,17 @@ class client(Thread):
                             for client in client_list:
                                 for clientChannel in client.channel:
                                     if(clientChannel == channel):
-                                        message = ':' + self.nickname + "!" + self.user + '@' + platform.node() + ' ' + line + '\n'
+                                        message = ':' + self.nickname + "!" + self.username + '@' + platform.node() + ' ' + line + '\n'
                                         client.sock.send(message.encode())
                         else:
                             message = ':' + host + ' 481 ' + self.nickname + ' :Permission Denied- You do not have access to this command\n'
                             self.sock.send(message.encode())        
 
-        except socket.error as e:
-            print("Error: " + str(e))
-            client_list.remove(self.sock)
+        except :
+            for client in client_list:
+                if client == self:
+                    client_list.remove(client)
+            
             self.sock.close()
 
 #Listen for client connections to server
@@ -265,6 +273,11 @@ serversocket.listen(5)
 print("Server is running and listening on port " + str(port))
 
 while True:
-    clientsocket, address = serversocket.accept()
-    client(clientsocket, address)
+    try:
+        clientsocket, address = serversocket.accept()
+        client(clientsocket, address)
+    except KeyboardInterrupt:
+        print("Server is shutting down")
+        serversocket.close()
+        break
         
