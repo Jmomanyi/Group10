69dbc3d2a3b5a0a019e7d716a42c408b4f31f2dc
Jmomanyi <jjjmomanyi@gmail.com>
2022-10-21 19:54:15 +0100

    jbjdbjf

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 #!/bin/python3
+
+
 import socket as s#import socket library
 import sys #imports sys library
 import os #imports os library
@@ -14,124 +16,98 @@ class bot():
         self.name=name
         self.nickname=nickname
         self.sock=s.socket(s.AF_INET, s.SOCK_STREAM)
-        #gets the host ip
-        self.host_ip=s.gethostbyname(s.gethostname())
-        #list of users in the channel
-        self.user_list=[self.name] 
-        #call connect to server function
-        self.connect_to_server()
-        #call join channel function
-        self.join_channel(self.channel)
-
-    #conect to server
+        self.host_ip=s.gethostbyname(s.gethostname())#get the host ip
+        self.user_list=[self.name] #list of users in the channel
+        self.connect_to_server()#call connect to server function
+        self.join_channel(self.channel)#call join channel function
+        #conect to server
     def connect_to_server(self):
         #try connecting to server and if it fails, print error and exit
         try:
             self.sock.connect((self.server, 6667))
-            print(f"Connected to {self.host_ip}")
+            print(f"connected to {self.host_ip}")
             
-            #sends nick name.
+            #identify itself
             self.sock.send(bytes("USER "+self.nickname+" "+self.nickname+" "+self.nickname+" :"+self.nickname+"\r\n", "UTF-8"))
             self.sock.send(bytes("NICK "+self.name+"\r\n", "UTF-8"))
         except s.error as e:
             #print error and exit
-            print(f"Error: {str(e)} Unable to connect to server.")
+            print(f"Error: {str(e)} unable to connect to server")
             sys.exit()
             
-    #join channel function.
+    """Join channel function receives two inputs the channel name and the socket"""
+    
     def join_channel(self, channel_name):
         #if channel is empty print to console channel empty and exit
         if channel_name=="":
-            print("Error: Channel name is empty.")
+            print("Error: channel name is empty")
             sys.exit(-1)
             
             #else try joining channel if failed print error and close socket
         else:
             try:#join channel
                 self.sock.send(bytes("JOIN "+channel_name+"\r\n", "UTF-8"))
-                #bot identifies itself.
+                #identify itself to the channel
                 self.sock.send(bytes("PRIVMSG "+channel_name+" :Hello, I am a"+self.nickname+" !\r\n", "UTF-8"))
-                print("Joined channel "+channel_name)
-            #print error and close socket if failed
+                print("joined channel "+channel_name)
+            #print error and close socket
             except s.error as e:
-                print("Error: "+str(e)+"Unable to join channel.")
+                print("Error: "+str(e)+"unable to join channel")
                 s.close()
                 
       
     #send message function
+    # receives the socket, destination and message as inputs then send the message to the required channel or destination
     def send_message(self,dest, message):
         #if destination is empty print error and exit
         if dest=="":
-            print("Error: Destination is empty.")
+            print("Error: destination is empty")
             sys.exit(-2)
             #if message is empty print error and exit
         if message=="":
-                print("Error: Message is empty.")
+                print("Error: message is empty")
                 sys.exit(-3)
-        #else try sending message if failed print error and close socket
+                #else try sending message if failed print error and close socket
         else:
             try:
                 #send message
                 self.sock.send(bytes("PRIVMSG "+dest+" :"+message+"\r\n", "UTF-8"))
                 #print error and close socket
             except s.error as e:
-                print("Error: "+str(e)+"Unable to send message.")
+                print("Error: "+str(e)+"unable to send message")
                 s.close()     
-
       
-    #gets name of the user who sent the message           
+      #get the name of the user who messaged           
     def  get_user(self,data):
-        #remove extra bits
-        msg=data.split('\n\r')
-        #split to obtain the name of the user
-        usr=msg[0].split(":")
-        #the name of the user is in the second index of the message received
-        us=usr[1] 
+        msg=data.split('\n\r')#remove extra bits
+        usr=msg[0].split(":")#split to obtain the name of the user
+        us=usr[1]#the name of the user is in the seond index of the mssg received
         if (self.name in usr[1])!=-1:
-                #if the name is not empty
-                if usr[1] != "":
-                    try:
-                        #get the name of the user who messaged and convert to string
-                        whotosend=str(usr[1].split('!')[0])
-                    except s.error as e:
-                        print("Error: "+str(e)+"Unable to get user.")
-                        s.close()
-        #return the name of the user
-        return whotosend 
-
-
+            if usr[1] != "":#if the name is not empty
+            #get the name of the user who messaged and convert to string
+              whotosend=str(usr[1].split('!')[0])
+              #return the name of the user
+        return whotosend      
     
-    
-    #adds user to the list of users
+    #got this from this website
+    #https://www.w3schools.com/python/trypython.asp?filename=demo_list_append
+    #add the user to the list of users
     def add_user(self,user):
-        #got this from this website
-        #https://www.w3schools.com/python/trypython.asp?filename=demo_list_append
         self.user_list.append(user)
-        #return self.user_list
         return self.user_list
-
     
-    #removes user from list if the user leaves the channel
+    #remve the user from list if the user leaves the channel
+#https://note.nkmk.me/en/python-list-clear-pop-remove-del/#:~:text=In%20Python%2C%20use%20list%20methods,with%20an%20index%20or%20slice.
     def remove_user(self,user):
-        #if user is not in the list
         if user not in self.user_list:
-            #prints user not in list
             print("User not in list")
-        #else remove user from list   
         else:
-            #https://note.nkmk.me/en/python-list-clear-pop-remove-del/#:~:text=In%20Python%2C%20use%20list%20methods,with%20an%20index%20or%20slice.
             self.user_list.remove(user)
             return self.user_list
 
-    def get_content(self,data):
-        msg=data.split('\n\r')
-        mess=msg[2].split(":")
-        
-    #gets the user_list and prints it
+#get the uselist
     def get_user_list(self):
         for user in self.user_list:
-            #got this from this website
-            #https://www.w3schools.com/python/trypython.asp?filename=demo_list_append
            user_list=[]
            user_list.append(user)
            print(user_list)
@@ -146,87 +122,83 @@ class bot():
         while True:
             
             
-            #try receiving messages from server if failed print server out and exit.
+            #try receiving messages from server if failed print error and close socket
             try:
                 data=self.sock.recv(1024).decode("UTF-8")
                 if not data:
-                    print("Server Out.")
+                    print("Server OUT")
                     break
-
                 #receives ping from the server and sends pong back to keep connection alive
                 if data.find("PING")!=-1:
                     self.sock.send(bytes("PONG "+data.split()[1]+"\r\n", "UTF-8"))
                     
-                    #prints the user list
+                    #print user list
                     print("*"*20)
-                    print("USERLIST.")
+                    print("USERLIST")
                     bot.get_user_list()  
                     print("*"*20)
 
 
 
-                #keeps track of users who JOIN channel
+                #keep track of users in channel who JOIN
                 if data.find("JOIN")!=-1:
-                    #gets the name of the user who joined
                     user=self.get_user(data)
-                    #adds user to the list
                     self.add_user(user)
                     print(user+" HAS JOINED CHANNEL.")
                     
-                    #print updated userlist
+                    #print user list
                     print("*"*20)
-                    print("USERLIST.")
+                    print("USERLIST")
                     bot.get_user_list()
                     print("*"*20)
 
-
-                #Keeps track of users who leave channel
+              #Keep track of u#Keep track of users in channel who PART
                 elif data.find("QUIT")!=-1:
-                    #gets the name of the user who left
                     user=self.get_user(data)
-                    #removes user from the list
                     bot.remove_user(user)
                     print("USER LEFT: "+user)
                     
                     #print updated userlist
                     print("*"*20)
-                    print("USERLIST.")
+                    print("USERLIST")
                     bot.get_user_list()
                     print("*"*20)
                 
                 
                         
-                     #respond to messages in channel
-                    #respond hello
-                    #provide help
-                    #roll a dice
-                    #slap a user
-                
+               #respond to messages in channel
+                #respond hello
+                #provide help
+                #roll a dice
+                #slap a user
+                if data.find("PRIVMSG")!=-1:
+                    whotosend=self.get_user(data)    
+                    messagetosend=rand.choice(list(open("facts.txt")))
+                    print(whotosend)
+                    self.send_message(whotosend,messagetosend)
                     
-                    #split data to get the message sent and print it
-                    message=bot.get_content(data)
+                     #else try sending message if failed print error and close socket     
+                 
+                    message=data.split("PRIVMSG",1)[1].split(":",1)[1]
                     print(message)
-
                     #if message starts with !
                     if message.startswith("!"):
                         #if a message starts with hello
                         if message.startswith("!hello"):
-                            #get the name of the user who messaged
-                            who=self.get_user(data)
-                            #get the time of the day
-                            time=os.popen("date").read()
+                            who=self.get_user(data)#get the name of the user who messaged
+                            time=os.popen("date").read()#get the time
                             
-                        #if a message starts with help
+                            #if a message starts with help
                         elif message.startswith("!help"):
                             #send list of commands
                             self.send_message(channel, "Commands: !hello, !help, !roll, !slap")
-                        #if a message starts with roll
+                            #if a message starts with roll
                         elif message.startswith("!roll"):
                             #choose a random number between 1 and 6
                             diceroll=rand.randint(1,6)
                             #send the random number
                             self.send_message(channel, f"You rolled a {diceroll}")
-                        #if a message starts with slap
+                            #if a message starts with slap
                         elif message.startswith("!slap"):
                             #get a random user from the list of users in the channel
                             randuser=rand.choice(self.user_list)
@@ -234,76 +206,60 @@ class bot():
                             while randuser==self.name:
                                 #get another random user
                                 randuser=rand.choice(self.user_list)
-                            #else
-                        else:     
-                                #slap a random user with a trout
-                                self.send_message(channel,"slaps"+" "+randuser+" "+"with a large trout. \n")
+                                #else
+                            else:     
+                               #slap a random user with a trout
+                              self.send_message(channel,"slaps"+" "+randuser+" "+"with a large trout. \n")
                                
                                
                                   
-                    else:   
+                        else:   
                             #reply random replies
-                            bot_replies.random_replies()
-                            
-                if data.find("PRIVMSG")!=-1:
-                    #gets name of user to send message to
-                    whotosend=self.get_user(data)  
-                    #gets the message to send  
-                    messagetosend=rand.choice(list(open("facts.txt")))
-                    print(whotosend)
-                    #sends message to user
-                    self.send_message(whotosend,messagetosend)
-                                    
-                            
-            #if failed, print error and exit.            
+                          bot_replies.random_replies()
+              #              
             except s.error as e:
-                print("Error: "+str(e)+"Unable to receive message.")
+                print("Error: "+str(e)+"unable to receive message")
                 s.close()
                 sys.exit() 
                 
                 
                 
                                      
-#class to handle bot replies                      
+  #class to handle bot replies                      
 class bot_replies():
-    
     def _init_(self,server,channel):
         self.server=server
         self.channel=channel
         self.sock=s.socket(s.AF_INET, s.SOCK_STREAM)
-
     #function to handle random replies when its not a command
     def random_replies():
-        #list of random replies
-       randlist=["Hello.",
-                    "How are you?",
-                    "Whoaaa!",
-                    "I am a bot.",
-                    "Hush!",
-                    "Cold.",
-                    "I get you.",
-                    "I understand.",
-                    "Will let you know.",]
-        #get a random message from the list
-       msg=rand.choice(randlist)
+       randlist=["hello",
+                    "how are you",
+                    "whoaaa",
+                    "I am a bot",
+                    "hush",
+                    "cold",
+                    "i get you",
+                    "i understand",
+                    "will let you know",
+                    
+                 ]
+       msg=rand.choice(randlist)#get a random message from the list
        #send the message to the channel
        bot.send_message(channel,msg)
      
 
        
     
-      
+#main function       
 if __name__=="__main__":
-    
-    #assigns the server,channel, name and nickname to variables
+ 
     server="127.0.0.1"
     channel="#BOTS"
     name="bot_peter"
     nickname="ruthlessbot"
+    
     print("#"*50)
-    #create an instance of the bot class and pass the server, channel, name and nickname as arguments.
     bot=bot(server, channel, name, nickname)
-    #call the main function
+
     bot.main()
-
-
